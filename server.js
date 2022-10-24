957f4d53376c5619a13393fea532263305a896db
tyana bandeira <tyanabandeira1@gmail.com>
2022-10-24 09:36:30 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+
+const server = http.createServer(function(req, res) {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/otherpage') {
+    fs.readFile('otherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/otherotherpage') {
+    fs.readFile('otherotherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/api') {
+    if('coinFlip' in params){
+     let betOutcome = Math.ceil(Math.random() * 2) === 0 ? 'heads' : 'tails' 
+      if(params['coinFlip']=== betOutcome['heads' || 'tails']){
+        result = betOutcome
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          flip: betOutcome
+        }
+        res.end(JSON.stringify(objToJson));
+      }//student = leon
+      else if(params['coinFlip'] != 'heads'){
+        result = betOutcome
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          flip: betOutcome,
+        }
+        res.end(JSON.stringify(objToJson));
+      }//student != leon
+    }//student if
+  }//else if
+  else if (page == '/css/style.css'){
+    fs.readFile('css/style.css', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+  }else if (page == '/js/main.js'){
+    fs.readFile('js/main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  }else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8000);
